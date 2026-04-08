a4205285b13b9e2a455914b3979936b70bb77dbc
Ankursh121 <ankurshakya321@gmail.com>
2026-04-09 01:15:42 +0530

    cleaning folder

--- test-chat.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3000/api/chat', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ messages: [{ role: 'user', content: 'hey' }] })
-}).then(async res => {
-  console.log("Status:", res.status);
-  const text = await res.text();
-  console.log("Body:", text);
-});
